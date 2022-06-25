e167648429e83b1fc1e6181c455610edef48d492
newodev <92925014+newodev@users.noreply.github.com>
2022-06-25 18:01:28 +0930

    begin some vulkan abstraction

--- Argent/Renderer/Descriptors.cpp
+++ Argent/Renderer/Descriptors.cpp
@@ -1,2 +1,47 @@
 #include <Descriptors.h>
 
+//////////// Descriptor Allocator ///////////////
+void ag::DescriptorAllocator::Initialise(vk::Device d)
+{
+	device = d;
+}
+
+void ag::DescriptorAllocator::Cleanup()
+{
+	for (vk::DescriptorPool p : freePools)
+		device.destroyDescriptorPool(p);
+
+	for (vk::DescriptorPool p : usedPools)
+		device.destroyDescriptorPool(p);
+}
+
+vk::DescriptorPool CreatePool(vk::Device device, const ag::DescriptorAllocator::PoolSizes& poolSizes, int count, vk::DescriptorPoolCreateFlags flags)
+{
+	std::vector<vk::DescriptorPoolSize> sizes;
+	sizes.reserve(poolSizes.Sizes.size());
+	for (auto e : poolSizes.Sizes)
+	{
+		sizes.push_back({ e.first, uint32_t(e.second * count) });
+	}
+
+	vk::DescriptorPoolCreateInfo info(flags, count, sizes);
+	return device.createDescriptorPool(info);
+}
+
+vk::DescriptorPool ag::DescriptorAllocator::GrabPool()
+{
+	// Use a free pool if possible
+	if (freePools.size() > 0)
+	{
+		vk::DescriptorPool free = freePools.back();
+		freePools.pop_back();
+
+		return free;
+	}
+	// Else we create a new one
+	else
+	{
+		return CreatePool(device, descriptorSizes, 1000, {});
+	}
+}
+
--- Argent/Renderer/Descriptors.h
+++ Argent/Renderer/Descriptors.h
@@ -1,9 +1,49 @@
 #pragma once
 
 #include <vulkan/vulkan.hpp>
+#include <vector>
+#include <array>
+#include <utility>
 
 // inspired by github.com/vblanco20-1
 namespace ag
 {
+	class DescriptorAllocator
+	{
+	public:
+		struct PoolSizes
+		{
+			std::vector<std::pair<vk::DescriptorType, float>> Sizes =
+			{
+				{ vk::DescriptorType::eSampler, 0.5f },
+				{ vk::DescriptorType::eCombinedImageSampler, 4.0f },
+				{ vk::DescriptorType::eSampledImage, 4.0f },
+				{ vk::DescriptorType::eStorageImage, 1.0f },
+				{ vk::DescriptorType::eUniformTexelBuffer, 1.0f },
+				{ vk::DescriptorType::eStorageTexelBuffer, 1.0f },
 
+				{ vk::DescriptorType::eUniformBuffer, 2.0f },
+				{ vk::DescriptorType::eUniformBufferDynamic, 1.0f },
+				{ vk::DescriptorType::eStorageBuffer, 2.0f },
+				{ vk::DescriptorType::eStorageBufferDynamic, 1.0f },
+
+				{ vk::DescriptorType::eInputAttachment, 0.5f }
+			};
+		};
+
+		void ResetPools();
+		bool Allocate(vk::DescriptorSet* set, vk::DescriptorSetLayout layout);
+
+		void Initialise(vk::Device d);
+		void Cleanup();
+
+	private:
+		vk::Device device;
+
+		vk::DescriptorPool GrabPool();
+		vk::DescriptorPool CurrentPool{ nullptr };
+		PoolSizes descriptorSizes;
+		std::vector<vk::DescriptorPool> usedPools;
+		std::vector<vk::DescriptorPool> freePools;
+	};
 }
